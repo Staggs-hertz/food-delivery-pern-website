a1e7f7d37668a5b90028df03eaf8ba2e57b7e2e8
Samuel Ogwuche <samogwuche98@gmail.com>
2025-12-11 10:26:58 +0100

    initial setup for server side

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+export default app;
---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import { pool } from "./config/db.js";
+
+const PORT = process.env.PORT;
+
+pool
+  .connect()
+  .then(() => {
+    console.log("Database Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection failed:", err);
+  });
